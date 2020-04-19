fa58f94f96ebf0aa171ef3443873defb5c158504
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-04-19 23:46:41 +0100

    cond rendering sorted - after quiz, questions disappear and category page shows instead

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Quiz from '../Quiz/index';
 
 function App() {
+  //state that holds the highest-counted category at end of quiz:
+  const [highestCat, setHighestCat] = useState('');
+  //state to check if quiz is done:
+  const [quizOver, setQuizOver] = useState(false);
+
   return (
     <div className="App">
-      <Quiz />
+      {!quizOver && (
+        <Quiz setHighestCat={setHighestCat} setQuizOver={setQuizOver} />
+      )}
+
+      {highestCat === 'animals' && (
+        <h1>Replace me with the component for the animals category page!</h1>
+      )}
+
+      {highestCat === 'environment' && (
+        <h1>
+          Replace me with the component for the environment category page!
+        </h1>
+      )}
+
+      {highestCat === 'localGroups' && (
+        <h1>
+          Replace me with the component for the localGroups category page!
+        </h1>
+      )}
+
+      {highestCat === 'events' && (
+        <h1>Replace me with the component for the events category page!</h1>
+      )}
     </div>
   );
 }
