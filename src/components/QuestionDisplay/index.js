db534b226cc335ad88b8da84750577c0c6c5f566
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-04-19 17:03:13 +0100

    questiondisplay component added

---
+++ src/components/QuestionDisplay/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function QuestionDisplay({ question, answerChoices }) {
+  return <div></div>;
+}
+
+export default QuestionDisplay;
--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -34,20 +34,23 @@
 
 //TODO: Render list of each question (map over question array and render value of each object)
 //TODO: Within map function of question, access the dummyAnswers array, picks the object with the right question number key, and then maps over the answer choices array, rendering each value
+//TODO: Once the map functions above are working, add a questionNumber state and conditional render to show one question at a time
 
 // **********************************************************
 
 import React, { useState, PropTypes } from 'react';
 
-const dummyQuestions = [
-  { q1: `What's your idea of a great way to spend a Saturday?` },
-  { q2: `It's film night! What are you watching?` },
-  { q3: `Let's have a dance. What's the soundtrack?` },
-  { q4: 'If you could teleport anywhere right now, where would you go?' },
-  { q5: 'What annoys you the most?' },
+import QuestionDisplay from '../QuestionDisplay/index';
+
+const questions = [
+  `1. What's your idea of a great way to spend a Saturday?`,
+  `2. It's film night! What are you watching?`,
+  `3. Let's have a dance. What's the soundtrack?`,
+  '4. If you could teleport anywhere right now, where would you go?',
+  '5. What annoys you the most?',
 ];
 
-const dummyAnswers = [
+const answers = [
   {
     q1: [
       { animals: 'Spending some quality time with my pet' },
@@ -112,12 +115,15 @@ function Quiz() {
   const [results, setResults] = useState([]);
 
   function addAnswer() {
-    setResults();
+    let newAnswer = '';
+    setResults([...results, newAnswer]);
   }
 
   return (
     <div>
-      <p>test</p>
+      {questions.map((question) => (
+        <QuestionDisplay question={question} />
+      ))}
     </div>
   );
 }
